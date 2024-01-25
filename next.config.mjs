fc33ef6549d30446bae213cb1dbf8beaa5641cf7
DiogoDuart3 <31131606+DiogoDuart3@users.noreply.github.com>
2024-01-25 21:17:13 +0000

    Update next.config.mjs with new image domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        hostname: "localhost",
-        pathname: "**",
-        port: "3000",
-        protocol: "http",
-      },
-    ],
+    domains: ["localhost", "digitalhippo-production-46ed.up.railway.app"],
   },
 };
 
